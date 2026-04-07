74fcd5a3abac1bbffee6fdce31d1f558296d387d
212-del <manjaykumars212@gmail.com>
2026-04-07 20:06:21 +0530

    Updated Functions

--- Cybersecurity-calculator.sh
+++ Cybersecurity-calculator.sh
@@ -519,6 +519,59 @@ case ${Choice1} in
 		 echo -n "Morse Code: "$output
          elif [[ "$mode" == "d" ]]; then
                  read -r -p " Enter Text to Decode : " decode
+		 declare -A MORSE
+
+		MORSE[".-"]="A"
+		MORSE["-..."]="B"
+		MORSE["-.-."]="C"
+		MORSE["-.."]="D"
+		MORSE["."]="E"
+		MORSE["..-."]="F"
+		MORSE["--."]="G"
+		MORSE["...."]="H"
+		MORSE[".."]="I"
+		MORSE[".---"]="J"
+		MORSE["-.-"]="K"
+		MORSE[".-.."]="L"
+		MORSE["--"]="M"
+		MORSE["-."]="N"
+		MORSE["---"]="O"
+		MORSE[".--."]="P"
+		MORSE["--.-"]="Q"
+		MORSE[".-."]="R"
+		MORSE["..."]="S"
+		MORSE["-"]="T"
+		MORSE["..-"]="U"
+		MORSE["...-"]="V"
+		MORSE[".--"]="W"
+		MORSE["-..-"]="X"
+		MORSE["-.--"]="Y"
+		MORSE["--.."]="Z"
+
+		MORSE["-----"]="0"
+		MORSE[".----"]="1"
+		MORSE["..---"]="2"
+		MORSE["...--"]="3"
+		MORSE["....-"]="4"
+		MORSE["....."]="5"
+		MORSE["-...."]="6"
+		MORSE["--..."]="7"
+		MORSE["---.."]="8"
+		MORSE["----."]="9"
+  		 output=""
+		 for token in $input; do
+		    if [[ "$token" == "/" ]]; then
+		        # Word separator
+		        output+=" "
+		    elif [[ -n "${MORSE[$token]}" ]]; then
+		        # Decode symbol
+		        output+="${MORSE[$token]}"
+		    else
+		        # Unknown pattern
+		        output+="?"
+		    fi
+		done
+		  echo -n "Decoded Text:"$output
 	 else
 		 echo "Invalid mode"
 	 fi
